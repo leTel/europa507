37d9a575f0afeb6d3e0d6167cc233c8f8701bfe0
lul <laurent.lugon@netplus.pro>
2015-04-14 12:00:56 +0200

    setting up local an heroku settings

--- e507/dev_settings.py
+++ e507/dev_settings.py
@@ -27,6 +27,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'e507_core',
+        'USER': 'e507_admin',
+        'PASSWORD': 'sNaEvs1MdAN',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 # Application definition
 
@@ -91,10 +104,6 @@ STATIC_URL = '/static/'
 
 # --- HEROKU ---
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -112,3 +121,4 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
