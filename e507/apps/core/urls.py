79d3f5cf5fd558803b42e71a2ac0bd0652cdbcd6
lul <laurent.lugon@netplus.pro>
2015-04-14 14:55:42 +0200

    navbar

--- e507/apps/core/urls.py
+++ e507/apps/core/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'e507.views.home', name='home'),
+    url(r'^$', 'e507.apps.news.views.home_view', name='home_url'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
