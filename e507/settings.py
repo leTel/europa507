635afa8bf968d70d38d10659c3fb3c30be33657a
lul <laurent.lugon@netplus.pro>
2015-10-13 17:00:41 +0200

    static fix

--- e507/settings.py
+++ e507/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    #os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
